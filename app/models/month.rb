c05324c7d41936844e91fdcc283eae7fa3a2ae14
Matt Berns <matthewl.berns@gmail.com>
2014-07-26 17:14:34 -0500

    Dayman created days

--- app/models/month.rb
+++ app/models/month.rb
@@ -4,6 +4,17 @@ class Month < ActiveRecord::Base
   after_create :createDays
 
   def createDays
+    #http://www.youtube.com/watch?v=0V784AiYtG8
+    if self.month_num == 1 || 3 || 5 || 7 || 8 || 10 || 12
+      days_in_month = 31
+    elsif self.month_num == 4 || 6 || 9 || 11
+      days_in_month = 30
+    elsif self.month_num == 2
+      days_in_month = 28
+    end
 
+    1.upto(days_in_month) do |date|
+      self.days << Day.create(date: date)
+    end
   end
 end
