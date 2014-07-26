4404aeacfebdf9c9d92e8c94de856840c8750bf9
Matt Berns <matthewl.berns@gmail.com>
2014-07-26 17:03:08 -0500

    Assigned month names upon creation

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -5,8 +5,9 @@ class Calendar < ActiveRecord::Base
 
   private
   def createMonths
+    month_names = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     1.upto(12) do |month_num|
-      self.months << months.create(month_num: month_num)
+      self.months << months.create(month_num: month_num, month_name: month_names[month_num])
     end
   end
   # use find or create by for assigning to user
--- app/models/month.rb
+++ app/models/month.rb
@@ -1,4 +1,9 @@
 class Month < ActiveRecord::Base
   has_many :days
   belongs_to :calendar
+  after_create :createDays
+
+  def createDays
+
+  end
 end
